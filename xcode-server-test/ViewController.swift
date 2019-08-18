de3e062e827561a0d37c88e7671e668960b4526c
tomoyukim <tomoyukim44@gmail.com>
2019-08-18 16:38:52 +0900

    first commit

---
+++ xcode-server-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xcode-server-test
+//
+//  Created by Tomoyuki Murakami on 2019/08/18.
+//  Copyright © 2019 tomoyukim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
